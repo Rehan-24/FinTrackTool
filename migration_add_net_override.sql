dd8493cbb5fae86a07ac2fc0568c628b9367d140
rkhan43 <rkhan43@gatech.edu>
2026-03-30 15:40:23 -0700

    cleaning up folder

--- migration_add_net_override.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add net_income_override to planning_overrides
--- This allows users to manually override the net income calculation
-
-ALTER TABLE public.planning_overrides 
-ADD COLUMN IF NOT EXISTS net_income_override NUMERIC(10, 2);
-
-COMMENT ON COLUMN public.planning_overrides.net_income_override IS 'Manual override for net income (if user wants different net than calculated)';
